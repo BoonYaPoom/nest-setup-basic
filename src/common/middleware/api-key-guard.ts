fc7a2f5d0f81a4096dc28272cfed4d66b97335b9
BoonYaPoom <101199599+BoonYaPoom@users.noreply.github.com>
2025-03-24 23:13:55 +0700

    000

--- src/common/middleware/api-key-guard.ts
+++ src/common/middleware/api-key-guard.ts
@@ -8,7 +8,9 @@ import {
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const request = context.switchToHttp().getRequest();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
     const apiKey = request.headers['x-api-key'];
 
     // ตรวจสอบว่า API key ถูกรวมอยู่ใน header และถูกต้องหรือไม่
